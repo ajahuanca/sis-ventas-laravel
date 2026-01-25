c88bcc6b11095cb4b01f50629e08dcc17aaa1018
Edwin Ajahuanca Callisaya <edwinajahuancacallisaya@gmail.com>
2026-01-25 03:28:44 -0400

    feat: Creacion del controlador HEALTH y su ruta

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,15 @@ use App\Http\Controllers\Api\ReporteController;
 use App\Http\Controllers\Api\RolController;
 use App\Http\Controllers\Api\UsuarioController;
 use App\Http\Controllers\Api\VentaController;
+use App\Http\Controllers\HealthController;
 use Illuminate\Support\Facades\Route;
+
+
+
+/*
+* API Route Health
+*/
+Route::get('/health', HealthController::class);
 
 /*
 |--------------------------------------------------------------------------

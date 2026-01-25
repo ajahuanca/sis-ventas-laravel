eeba57a013d41d3d7218f0e001de9330bb0435fb
Edwin Ajahuanca Callisaya <edwinajahuancacallisaya@gmail.com>
2026-01-25 00:27:58 -0400

    Para nuevo repositorio

--- vite.config.js
+++ vite.config.js
@@ -24,6 +24,12 @@ export default defineConfig({
     },
     server: {
         host: '127.0.0.1',
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: 'localhost',
+            clientPort: 5173,
+        },
         watch: {
             ignored: ["**/storage/framework/views/**"],
         },

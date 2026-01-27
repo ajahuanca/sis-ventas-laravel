7ef017584dddb2395d329e6352c68b0989c72b91
Edwin Ajahuanca Callisaya <edwinajahuancacallisaya@gmail.com>
2026-01-27 01:01:24 -0400

    test: Corrección de importacion correcta del archivo App.vue en el archivo test

--- resources/js/test/App.test.js
+++ resources/js/test/App.test.js
@@ -1,11 +1,11 @@
 import { mount } from '@vue/test-utils';
 import { describe, it, expect } from 'vitest';
-import App from '../components/App.vue';
+import App from "../App.vue";
 
 describe('App', () => {
   it('renderizar mensaje', () => {
-    const wrapper = mount(App, { props: { msg: 'Hola, verificacion de test' } });
-    expect(wrapper.text()).toContain('Hola, verificacion de test');
+    const wrapper = mount(App, { props: { msg: 'Ingresa tus credenciales' } });
+    expect(wrapper.text()).toContain('Ingresa tus credenciales para continuar');
   });
 });
 
